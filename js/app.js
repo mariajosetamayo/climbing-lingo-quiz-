d36b1153aed2243055312da63175816412a55cf7
Maria Jose Tamayo y Ortiz <mariajose.tamayo@Marias-Mac-mini.local>
2016-08-24 16:57:13 -0600

    version with styling, comments still missing

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 
 	$(".startButton").click(function(){
@@ -73,7 +74,7 @@ $(document).ready(function(){
 
 	
 	function nextQuestion () {
-		
+
 		$('input[name=option]').attr('checked',false);
 		document.getElementById("count").innerHTML = counter + 1
 		totalPoints = totalPoints + currentAnswerPoints
@@ -81,6 +82,7 @@ $(document).ready(function(){
 		document.getElementById("score").innerHTML = totalPoints
 		var presentQuestion = new Question (questions[counter], options[counter], answers[counter])
 		console.log("question number", presentQuestion)
+		
 		if(counter <10){
 			document.getElementById("question").innerHTML = presentQuestion.question
 			document.getElementById("option0text").innerHTML = presentQuestion.options[0]
@@ -140,7 +142,7 @@ $(document).ready(function(){
 			$(".master").hide()
 			$(".ocassional").hide()
 		}
-		else if (totalPoints >3 && totalPoints <= 6){
+		else if (totalPoints >3 && totalPoints <= 8){
 			$(".ocassional").show()
 			$(".climbingRookie").hide()
 			$(".gymRat").hide()
@@ -153,137 +155,5 @@ $(document).ready(function(){
 			$(".ocassional").hide()
 		}
 	}
-
-
-
-
-	// $("#resultButton").click(function(){
-	// 	if (true) {};
-	// })
-
-	
-	// var Questions = [
-	// 	{
-	// 		question: "What is a sandbag?",
-	// 		options: ["An easy climb", "A horrendously hard climb", "A climb which receives a much lower grade than deserved", "A climb which receives a much higher grade than deserved"],
-	// 		answerChoiceIndex: 2
-	// 	},
-	// 	{
-	// 		question: "The crux of a climb is:",
-	// 		options: ["the end of a very long climb","the hardest part of a climb","the easiest part of a climb", "the part where you were stronger in the climb"],
-	// 		answerChoiceIndex: 1
-	// 	},
-	// 	{
-	// 		question: "What is free climbing?",
-	// 		options: ["to climb without a rope","Climbing without unnatural aids other than used for protection","Climbing a route alone with an auto-belay","Climbing without natural aids used for protection"],
-	// 		answerChoiceIndex: 1
-	// 	},
-
-	// 	{
-	// 		question: "Which is the best description for a crimp?",
-	// 		options:["a horrendously small and slippery hold" ,"A very large hold that you can grab easily", "A hold which is only just big enough to be grasped with the tips of the fingers", "A rounded and slippery hold"],
-	// 		answerChoiceIndex: 2
-	// 	},
-
-	// 	{
-	// 		question: "What is a flash?",
-	// 		options:["to successfully complete a climbing route without falling on the first attempt after receiving beta of some form", "To fall off a climbing route too quickly", "to successfully complete a route without falling", "To complete a climbing route very quickly"],
-	// 		answerChoiceIndex: 0
-	// 	},
-
-	// 	{
-	// 		question: "When the climber shouts at a belayer 'take!', he is:",
-	// 		options:["requesting the belayer to go back down","requesting the belayer to give them more slack", "requesting to take a break", "requesting that the belayer removes all slack"],
-	// 		answerChoiceIndex: 3
-	// 	},
-
-	// 	{
-	// 		question: "What is a redpoint?",
-	// 		options:["completing a climb successfully without falling while on a top rope after making previous unsuccessful attempts, done without falling or resting on the rope","To complete a climb while placing protection on a lead climb after making previous unsuccessful attempts, done without falling or resting on the rope",
-	// 		"To complete a lead climb after making previous unsuccessful attempts, done without falling or resting on the rope","To complete a lead climb on the first attempt without falling"],
-	// 		answerChoiceIndex: 1
-	// 	},
-
-	// 	{
-	// 		question: "What is flagging?",
-	// 		options:["Climbing technique where a leg is held in a position to maintain balance, rather than to support weight", "a technique to barndoor and climb more dynamically","a technique to prepare yourself for jumping onto another hold",
-	// 		"To use friction on the sole of the climbing shoe, in the absence of any useful footholds"],
-	// 		answerChoiceIndex: 0
-	// 	},
-
-	// 	{
-	// 		question: "What is talus hopping?",
-	// 		options:["Jumping from the highest part of a boulder problem to the ground", "A technique that is typically used while lowering and cleaning gear from an overhanging and/or traversing route", 
-	// 		"Jumping or transitioning from rock to rock in an area of large rock fragments on a mountainside that may vary from house-size to as small as a small backpack", "Scrambling to get to the top of an oddly shaped boulder"],
-	// 		answerChoiceIndex: 2
-	// 	},
-
-	// 	{
-	// 		question: "What is a whipper?",
-	// 		options:["When the rope slashes a part of your body",  "falling to the ground while leading", "Getting rope burnt", "any fall beyond the last placed or clipped piece of protection"],
-	// 		answerChoiceIndex: 3
-	// 	},
-
-	// ];
-
-	// var Quiz = {
-
- //  		score: 0,
- //  		questions: [],
- //  		currentQuestionIndex: 0,
- //  		currentQuestion: function() {
- //    		return this.questions[this.currentQuestionIndex]
- //  		}
-	// };
-
-	// // creating a variable to identify current questions
-
-	// var quiz = Object.create(Quiz);
-	// quiz.questions = Questions;
-	// var q = quiz.currentQuestion();
-
-	// function updateCup() {
- //        var answer = $("input[type='radio']:checked").val();
- //        if (answer == questions[currentQuestion].correct) {
- //            numberCorrect++;    
- //        }
- //        if (numberCorrect == 1) {
- //            $(".score_cup").css("display", "none")
- //            $("#score_cup1").fadeIn();
- //        }
- //        else if (numberCorrect == 2) {
- //            $(".score_cup").css("display", "none")
- //            $("#score_cup2").fadeIn();
- //        }
- //        else if (numberCorrect == 3) {
- //            $(".score_cup").css("display", "none")
- //            $("#score_cup3").fadeIn();
- //        }
- //        else if (numberCorrect == 4) {
- //            $(".score_cup").css("display", "none")
- //            $("#score_cup4").fadeIn();
- //        }
- //        else if (numberCorrect == 5) {
- //            $(".score_cup").css("display", "none")
- //            $("#score_cup5").fadeIn();
- //        }
- //    }
-	// $(".startButton").click(function(){
-	// 	$("")
-			
-	// }
-		
-	// })
-	
-
-
-
-	// var arrayQuestions = ["What is a sandbag?", "What is a flash?", "What is free climbing?","The crux of a climb is:", "Which is the best description for a crimp?", "What is flagging?",
-	// "When the climber shouts at a belayer take!, he is:", "What is a whipper?","What is talus hopping?","What is a redpoint?" ]
-
-	// var arrayOptions = [["a","b","c","d"]]
-
-
-
 
 })
